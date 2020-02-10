9dc10e34b1a75de7d164b3c1a7b3173cb75d3426
FireSoul512 <183386@ids.upchiapas.edu.mx>
2020-02-10 10:15:50 -0600

    Update urls.py

--- Login/urls.py
+++ Login/urls.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 # from rest_framework import routers, serilizers, viewsets
 from Login.views import CustonAuthToken
 
+from Login import views
+
 urlpatterns = [
     re_path(r'^', CustonAuthToken.as_view()),
+
+    re_path(r'Login_list2/$', views.ExampleList2.as_view()),
+    #re_path(r'Login_detail2/$', views.ExampleDetail2.as_view()),
 ]
