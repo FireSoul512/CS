0bcb7b5e49032cdd9c2c3e90eb8aa9407bc2b8c3
FireSoul512 <183386@ids.upchiapas.edu.mx>
2020-02-10 08:39:33 -0600

    Update settings.py

--- CS/settings.py
+++ CS/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+7_i4cc&1+0x=mghhzoxrfd2r@ek1bd^bl42q%k$q2og&7oy(d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['localhost','54.211.8.177', '34.226.119.99']
 
@@ -94,10 +96,10 @@ WSGI_APPLICATION = 'CS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jorge',
-        'USER': 'postgres',
-        'PASSWORD': 'DanielaEmili512',
-        'HOST': 'jorgeliy.czbgrhdgcvto.us-east-1.rds.amazonaws.com',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
     }
 }
