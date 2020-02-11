c9f676f9f9187f3f282c26021411727d99a2c173
FireSoul512 <183386@ids.upchiapas.edu.mx>
2020-02-11 15:52:44 -0600

    Update serializer.py

--- Login/serializer.py
+++ Login/serializer.py
@@ -5,4 +5,4 @@ from Login.models import Example2
 class Example2Serializers(serializers.ModelSerializer):
     class Meta:
         model = Example2
-        field = ('__all__')
+        fields = ('__all__')
